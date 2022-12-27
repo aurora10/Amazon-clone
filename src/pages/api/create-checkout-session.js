d3625ce608409103746f2102deeee47fc3bffbbc
aurora10 <aurora10@gmail.com>
2022-12-27 11:37:29 +0100

    new shipping rate

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,51 +1,40 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
-
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-    const  {
-        items,
-        email
-    } = req.body;
+  const { items, email } = req.body;
 
+  const transformedItems = items.map((item) => ({
+    quantity: 1,
+    price_data: {
+      currency: "usd",
+      unit_amount: item.price * 100,
+      product_data: {
+        name: item.title,
+        images: [item.image],
+        description: item.description,
+      },
+    },
+  }));
 
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    //shipping_rates: [''],
+    shipping_options: [{ shipping_rate: "shr_1MJaPRL1z8EG1fYgr2pMJGHk" }],
 
-    const transformedItems = items.map((item) => ({
-        
-        quantity: 1,
-        price_data: {
-            currency: 'usd',
-            unit_amount: item.price * 100,
-            product_data: {
-                name: item.title,
-                images: [item.image],
-                description: item.description,
-            },
-        },
-    }));
+    shipping_address_collection: {
+      allowed_countries: ["GB", "US", "CA"],
+    },
+    line_items: transformedItems,
+    mode: "payment",
+    success_url: `${process.env.HOST}/success`,
+    cancel_url: `${process.env.HOST}/checkout`,
+    metadata: {
+      email,
+      images: JSON.stringify(items.map((item) => item.image)),
+    },
+  });
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        //shipping_rates: [''],
-        shipping_options: [{shipping_rate: 'shr_1MFhHmL1z8EG1fYglFH9EBP7'}],
-           
-        shipping_address_collection: {
-            allowed_countries: ['GB', 'US', 'CA']
-        },
-        line_items: transformedItems,
-        mode: 'payment',
-        success_url: `${process.env.HOST}/success`,
-        cancel_url: `${process.env.HOST}/checkout`,
-        metadata: {
-            email,
-            images: JSON.stringify(items.map((item) => item.image))
-        }
-
-    });
-
-    res.status(200).json({
-        id: session.id
-    })
-
-
-}
+  res.status(200).json({
+    id: session.id,
+  });
+};
